cdfd527b664198082193f0c1604f41b28c213d76
Nils Moehrle <nils@kuenstle-moehrle.de>
2016-10-19 15:04:20 +0300

    Fix transform inversion - consider scale

--- apps/mesh_tools/convert.cpp
+++ apps/mesh_tools/convert.cpp
@@ -134,17 +134,21 @@ int main(int argc, char **argv) {
             R(0, 0) = T(0, 0); R(0, 1) = T(1, 0); R(0, 2) = T(2, 0);
             R(1, 0) = T(0, 1); R(1, 1) = T(1, 1); R(1, 2) = T(2, 1);
             R(2, 0) = T(0, 2); R(2, 1) = T(1, 2); R(2, 2) = T(2, 2);
-            math::Vec3f t = -R * math::Vec3f(T(0, 3), T(1, 3), T(2, 3));
-            T = R.hstack(t).vstack(math::Vec4f(0.0f, 0.0f, 0.0f, 1.0f));
+            math::Matrix3f S(0.0f);
+            S(0, 0) = 1.0f / R.row(0).norm();
+            S(1, 1) = 1.0f / R.row(1).norm();
+            S(2, 2) = 1.0f / R.row(2).norm();
+            R = S * R;
+            math::Vec3f t = -R * S * math::Vec3f(T(0, 3), T(1, 3), T(2, 3));
+            T = (R * S).hstack(t).vstack(math::Vec4f(0.0f, 0.0f, 0.0f, 1.0f));
         }
-
 
         for (std::size_t i = 0; i < vertices.size(); ++i) {
             vertices[i] = T.mult(vertices[i], 1.0f);
         }
 
         for (std::size_t i = 0; i < normals.size(); ++i) {
-            normals[i] = T.mult(normals[i], 0.0f);
+            normals[i] = T.mult(normals[i], 0.0f).normalize();
         }
     }
 
