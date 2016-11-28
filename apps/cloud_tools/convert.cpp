7ea6716c062608a3756f4a51c2f2fc1022b6d5fe
Nils Moehrle <nils@kuenstle-moehrle.de>
2016-11-28 19:55:37 +0100

    Introduce --bounding-box to convert-cloud

--- apps/cloud_tools/convert.cpp
+++ apps/cloud_tools/convert.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "math/octree_tools.h"
+
 #include "util/system.h"
 #include "util/arguments.h"
+#include "util/tokenizer.h"
 
 #include "mve/mesh_io.h"
 
@@ -10,7 +13,30 @@
 struct Arguments {
     std::string in_cloud;
     std::string out_cloud;
+    std::string aabb;
 };
+
+
+/* Derived from mve/apps/scene2pset/scene2pset.cc */
+void
+aabb_from_string (std::string const& str,
+    math::Vec3f* aabb_min, math::Vec3f* aabb_max)
+{
+    util::Tokenizer tok;
+    tok.split(str, ',');
+    if (tok.size() != 6) {
+        std::cerr << "Error: Invalid AABB given" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        (*aabb_min)[i] = tok.get_as<float>(i);
+        (*aabb_max)[i] = tok.get_as<float>(i + 3);
+    }
+    std::cout << "Using AABB: (" << (*aabb_min) << ") / ("
+        << (*aabb_max) << ")" << std::endl;
+}
+
 
 Arguments parse_args(int argc, char **argv) {
     util::Arguments args;
@@ -18,6 +44,7 @@ Arguments parse_args(int argc, char **argv) {
     args.set_nonopt_minnum(2);
     args.set_nonopt_maxnum(2);
     args.set_usage("Usage: " + std::string(argv[0]) + " [OPTS] IN_CLOUD OUT_CLOUD");
+    args.add_option('b', "bounding-box", true, "Six comma separated values used as AABB.");
     args.set_description("Converts point cloud formats");
     args.parse(argc, argv);
 
@@ -28,6 +55,7 @@ Arguments parse_args(int argc, char **argv) {
     for (util::ArgResult const* i = args.next_option();
          i != 0; i = args.next_option()) {
         switch (i->opt->sopt) {
+        case 'b': conf.aabb = i->arg; break;
         default:
             throw std::invalid_argument("Invalid option");
         }
@@ -36,11 +64,38 @@ Arguments parse_args(int argc, char **argv) {
     return conf;
 }
 
+template <typename T>
+void remove_elements(std::vector<T> * vec, std::vector<bool> del)
+{
+    if (vec->empty()) return;
+
+    typename std::vector<T>::iterator r, w;
+    std::size_t i = 0;
+
+    /* Advance till first item to delete. */
+    while (!del[i]) ++i;
+
+    r = w = vec->begin() + i;
+
+    for (; i < vec->size(); ++i, ++r) {
+        if (del[i]) continue;
+
+        *w++ = *r;
+    }
+
+    vec->erase(w, vec->end());
+}
+
 int main(int argc, char **argv) {
     util::system::register_segfault_handler();
     util::system::print_build_timestamp(argv[0]);
 
     Arguments args = parse_args(argc, argv);
+
+    math::Vec3f min, max;
+    if (!args.aabb.empty()) {
+        aabb_from_string(args.aabb, &min, &max);
+    }
 
     std::string ext = util::string::right(args.in_cloud, 3);
 
@@ -54,6 +109,20 @@ int main(int argc, char **argv) {
     } catch (std::exception& e) {
         std::cerr << "Could not load mesh: " << e.what() << std::endl;
         std::exit(EXIT_FAILURE);
+    }
+
+    if (!args.aabb.empty()) {
+        std::vector<math::Vec3f> const & verts = mesh->get_vertices();
+        std::vector<bool> del(verts.size(), false);
+
+        for (std::size_t i = 0; i < verts.size(); ++i) {
+            del[i] = !math::geom::point_box_overlap(verts[i], min, max);
+        }
+
+        remove_elements(&mesh->get_vertices(), del);
+        remove_elements(&mesh->get_vertex_values(), del);
+        remove_elements(&mesh->get_vertex_colors(), del);
+        remove_elements(&mesh->get_vertex_confidences(), del);
     }
 
     mve::geom::save_mesh(mesh, args.out_cloud);
