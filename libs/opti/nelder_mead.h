86544f968135d6e5075d939a71167823d4cf83d3
Nils Moehrle <nils@kuenstle-moehrle.de>
2016-11-30 19:57:47 +0100

    Add Nelder-Mead implementation

---
+++ libs/opti/nelder_mead.h
@@ -0,0 +1,114 @@
+#include <array>
+#include <functional>
+#include <algorithm>
+
+#include "math/vector.h"
+
+template <int N>
+struct Simplex {
+    std::array<math::Vector<float, N>, N + 1> verts;
+};
+
+template <int N>
+void
+shrink(Simplex<N> * simplex, math::Vector<float, N> towards) {
+    for (int i = 0; i < N + 1; ++i) {
+        math::Vector<float, N> & vert = simplex->verts[i];
+        vert = towards + (vert - towards) * 0.5f;
+    }
+}
+
+template <int N>
+std::pair<std::size_t, float>
+nelder_mead(Simplex<N> * simplex, std::function<float(math::Vector<float, N>)> const & func) {
+    std::array<float, N + 1> values;
+
+    std::array<math::Vector<float, N>, N + 1> & verts = simplex->verts;
+    for (int k = 0; k < values.size(); ++k) {
+        values[k] = func(verts[k]);
+    }
+
+    std::vector<int> ranks(values.size());
+    std::iota(ranks.begin(), ranks.end(), 0);
+    std::sort(ranks.begin(), ranks.end(),
+        [&values] (std::size_t a, std::size_t b) {
+            return values[a] < values[b];
+        }
+    );
+
+    std::size_t best_idx = ranks.front();
+    std::size_t lousy_idx = ranks[ranks.size() - 2];
+    std::size_t worst_idx = ranks.back();
+
+    float best_value = values[best_idx];
+    float lousy_value = values[lousy_idx];
+    float worst_value = values[worst_idx];
+
+    math::Vector<float, N> hpc(0.0f);
+    for (int k = 0; k < ranks.size() - 1; ++k) {
+        hpc += verts[ranks[k]];
+    }
+    hpc /= N;
+
+    math::Vector<float, N> refl = hpc + (hpc - verts[worst_idx]);
+
+    float refl_value = func(refl);
+    float value = 0;
+
+    if (refl_value < best_value) {
+        /* Expansion */
+        math::Vector<float, N> exp = refl + (refl - hpc);
+        float exp_value = func(exp);
+
+        if (exp_value < best_value) {
+            verts[worst_idx] = exp;
+            return {worst_idx, exp_value};
+        } else {
+            verts[worst_idx] = refl;
+            return {worst_idx, refl_value};
+        }
+    } else {
+        if (refl_value < worst_value) {
+            if (refl_value < lousy_value) {
+                verts[worst_idx] = refl;
+                return {best_idx, best_value};
+            } else {
+                /* Outside contraction */
+                math::Vector<float, N> con = hpc + (hpc - verts[worst_idx]) * 0.5f;
+                float con_value = func(con);
+
+                if (con_value < worst_value) {
+                    verts[worst_idx] = con;
+                    if (con_value < best_value) {
+                        return {worst_idx, con_value};
+                    } else {
+                        return {best_idx, best_value};
+                    }
+                } else {
+                    /* Shrink */
+                    shrink(simplex, verts[best_idx]);
+                    value = best_value;
+                    return {best_idx, best_value};
+                }
+            }
+        } else {
+            /* Inside contraction */
+            math::Vector<float, N> con = hpc - (hpc - verts[worst_idx]) * 0.5f;
+            float con_value = func(con);
+
+            if (con_value < worst_value) {
+                verts[worst_idx] = con;
+                if (con_value < best_value) {
+                    return {worst_idx, con_value};
+                } else {
+                    return {best_idx, best_value};
+                }
+            } else {
+                /* Shrink */
+                shrink(simplex, verts[best_idx]);
+                return {best_idx, best_value};
+            }
+        }
+    }
+}
+
---
+++ libs/opti/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "nelder_mead.h"
+
+#define SQR(x) ((x) * (x))
+
+float sqr(float x) {
+    return x * x;
+}
+
+int main(int argc, char * argv[]) {
+    std::function<float(math::Vector<float, 2>)> f = [] (math::Vec2f x) -> float {
+        return SQR(1.0f - x[0]) + 100.0f * sqr(x[1] - SQR(x[0]));
+    };
+
+    Simplex<2> simplex;
+    simplex.verts[0] = math::Vec2f(-1.3f, -1.3f);
+    simplex.verts[1] = math::Vec2f(-1.2f, -1.2f);
+    simplex.verts[2] = math::Vec2f(-1.2f, -1.4f);
+
+    for (int i = 0; i < 50; ++i) {
+        std::size_t idx;
+        float value;
+        std::tie(idx, value) = nelder_mead(&simplex, f);
+        std::cout << simplex.verts[idx] << std::endl;
+        std::cerr << value << std::endl;
+    }
+}
