b635829985ff21097dd00db5ed7172fc13999769
Nils Moehrle <nils@kuenstle-moehrle.de>
2017-02-21 14:16:11 +0100

    Add ability to reduce number of vertices (convert-cloud)

--- apps/cloud_tools/convert.cpp
+++ apps/cloud_tools/convert.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <random>
 #include <fstream>
+#include <iostream>
 #include <iterator>
+#include <algorithm>
 
 #include "math/octree_tools.h"
 
@@ -16,6 +18,7 @@ struct Arguments {
     std::string in_cloud;
     std::string out_cloud;
     std::string aabb;
+    float fraction;
 };
 
 Arguments parse_args(int argc, char **argv) {
@@ -25,17 +28,20 @@ Arguments parse_args(int argc, char **argv) {
     args.set_nonopt_maxnum(2);
     args.set_usage("Usage: " + std::string(argv[0]) + " [OPTS] IN_CLOUD OUT_CLOUD");
     args.add_option('b', "bounding-box", true, "Six comma separated values used as AABB.");
+    args.add_option('r', "reduce", true, "Reduce to fraction of vertices [1.0].");
     args.set_description("Converts point cloud formats");
     args.parse(argc, argv);
 
     Arguments conf;
     conf.in_cloud = args.get_nth_nonopt(0);
     conf.out_cloud = args.get_nth_nonopt(1);
+    conf.fraction = 1.0f;
 
     for (util::ArgResult const* i = args.next_option();
          i != 0; i = args.next_option()) {
         switch (i->opt->sopt) {
         case 'b': conf.aabb = i->arg; break;
+        case 'r': conf.fraction = i->get_arg<float>(); break;
         default:
             throw std::invalid_argument("Invalid option");
         }
@@ -109,6 +115,26 @@ int main(int argc, char **argv) {
     } catch (std::exception& e) {
         std::cerr << "Could not load mesh: " << e.what() << std::endl;
         std::exit(EXIT_FAILURE);
+    }
+
+    if (args.fraction != 1.0f) {
+        std::vector<math::Vec3f> const & verts = mesh->get_vertices();
+        std::vector<std::size_t> idx(verts.size());
+        std::iota(idx.begin(), idx.end(), 0);
+        std::mt19937 g;
+        std::shuffle(idx.begin(), idx.end(), g);
+
+        std::size_t max_idx = verts.size() * args.fraction;
+
+        std::vector<bool> del(verts.size(), false);
+        for (std::size_t i = 0; i < verts.size(); ++i) {
+            del[i] = idx[i] > max_idx;
+        }
+
+        remove_elements(&mesh->get_vertices(), del);
+        remove_elements(&mesh->get_vertex_values(), del);
+        remove_elements(&mesh->get_vertex_colors(), del);
+        remove_elements(&mesh->get_vertex_confidences(), del);
     }
 
     if (!args.aabb.empty()) {
