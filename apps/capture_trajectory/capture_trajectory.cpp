a870428425756b0cedd8a318ad81c06b8569ffd9
Nils Moehrle <nils@kuenstle-moehrle.de>
2017-03-01 17:45:20 +0100

    Change defaults of capture trajectory

--- apps/capture_trajectory/capture_trajectory.cpp
+++ apps/capture_trajectory/capture_trajectory.cpp
@@ -22,8 +22,8 @@ struct Arguments {
     std::string scene_dir;
     std::string model;
     std::string image_name = "original";
-    int width = 1920;
-    int height = 1080;
+    int width = 1404;
+    int height = 936;
 };
 
 Arguments parse_args(int argc, char **argv) {
@@ -34,7 +34,7 @@ Arguments parse_args(int argc, char **argv) {
     args.set_helptext_indent(28);
     args.set_usage("Usage: " + std::string(argv[0]) + " [OPTS] SCENE MODEL");
     args.set_description("Renders the model from all trajectory views given as scene");
-    args.add_option('r', "resolution", true, "resolution [1920x1080]");
+    args.add_option('r', "resolution", true, "resolution [1404x936]");
     args.parse(argc, argv);
 
     Arguments conf;
@@ -59,61 +59,32 @@ Arguments parse_args(int argc, char **argv) {
     return conf;
 }
 
-void
-fill_ogl_camera(mve::CameraInfo const & camera_info, int width, int height, float znear, float zfar, ogl::Camera * ogl_camera) {
-    /* Get all parameters and check them. */
-    float const dimension_aspect = static_cast<float>(width) / height;
-    float const pixel_aspect = camera_info.paspect;
-    float const image_aspect = dimension_aspect * pixel_aspect;
-    float const focal_length = camera_info.flen;
-    float const ppx = camera_info.ppoint[0];
-    float const ppy = camera_info.ppoint[1];
-
-    /* Fill OpenGL view matrix */
-    camera_info.fill_world_to_cam(ogl_camera->view.begin());
-    camera_info.fill_cam_to_world(ogl_camera->inv_view.begin());
-
-    /* Construct OpenGL projection matrix. */
-    math::Matrix4f& proj = ogl_camera->proj;
-    proj.fill(0.0f);
-    proj[0] = 2.0f * focal_length
-        * (image_aspect > 1.0f ? 1.0f : 1.0f / image_aspect);
-    proj[2] = -2.0f * (0.5f - ppx);
-    proj[5] = -2.0f * focal_length
-        * (image_aspect > 1.0f ? image_aspect : 1.0f);
-    proj[6] = -2.0f * (ppy - 0.5f);
-    proj[10] = -(-zfar - znear) / (zfar - znear);
-    proj[11] = -2.0f * zfar * znear / (zfar - znear);
-    proj[14] = 1.0f;
-
-    camera_info.fill_camera_pos(ogl_camera->pos.begin());
-
-    ogl_camera->z_near = znear;
-    ogl_camera->z_far = zfar;
-}
-
 void setup_fbo(GLuint *fbo, GLuint *rbo_color, GLuint *rbo_depth, int msaa_samples, int width, int height)
 {
     /* Color renderbuffer. */
     glGenRenderbuffers(1, rbo_color);
     glBindRenderbuffer(GL_RENDERBUFFER, *rbo_color);
-    glRenderbufferStorageMultisample(GL_RENDERBUFFER, msaa_samples, GL_RGBA32F, width, height);
+    glRenderbufferStorageMultisample(GL_RENDERBUFFER, msaa_samples,
+        GL_RGBA32F, width, height);
     ogl::check_gl_error();
     glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
     /* Depth renderbuffer. */
     glGenRenderbuffers(1, rbo_depth);
     glBindRenderbuffer(GL_RENDERBUFFER, *rbo_depth);
-    glRenderbufferStorageMultisample(GL_RENDERBUFFER, msaa_samples, GL_DEPTH_COMPONENT32F, width, height);
+    glRenderbufferStorageMultisample(GL_RENDERBUFFER, msaa_samples,
+        GL_DEPTH_COMPONENT32F, width, height);
     ogl::check_gl_error();
     glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
     /* Framebuffer */
     glGenFramebuffers(1, fbo);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, *fbo);
-    glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, *rbo_color);
+    glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+        GL_RENDERBUFFER, *rbo_color);
     ogl::check_gl_error();
-    glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, *rbo_depth);
+    glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+        GL_RENDERBUFFER, *rbo_depth);
     ogl::check_gl_error();
     GLenum fb_status = glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER);
     if (fb_status != GL_FRAMEBUFFER_COMPLETE) {
@@ -175,7 +146,10 @@ int main(int argc, char **argv) {
         ogl::Camera ogl_cam;
         float const znear = 0.1f;
         float const zfar = 1000.0f;
-        fill_ogl_camera(view->get_camera(), width, height, znear, zfar, &ogl_cam);
+
+        mve::CameraInfo const & cam = view->get_camera();
+        cam.fill_gl_viewtrans(ogl_cam.view.begin());
+        cam.fill_gl_projection(ogl_cam.proj.begin(), width, height, znear, zfar);
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -185,7 +159,8 @@ int main(int argc, char **argv) {
         /* Bind multisampled framebuffer image to default framebuffer. */
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo_final);
         glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
-        glBlitFramebuffer(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+        glBlitFramebuffer(0, 0, width, height,
+            0, 0, width, height, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT, GL_NEAREST);
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
         glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo_final);
 
