f03d7373feadcbc5548620c6bf840d28f73ae998
Nils Moehrle <nils@kuenstle-moehrle.de>
2017-03-05 15:04:53 +0100

    Add cleaning to convert-mesh

--- apps/mesh_tools/convert.cpp
+++ apps/mesh_tools/convert.cpp
@@ -5,8 +5,8 @@
 
 #include "util/matrix_io.h"
 
+#include "mve/mesh_tools.h"
 #include "mve/mesh_io_ply.h"
-#include "mve/bundle_io.h"
 
 #include "acc/primitives.h"
 
@@ -22,8 +22,8 @@ struct Arguments {
     std::string transform;
     mve::geom::SavePLYOptions opts;
     bool invert;
+    bool clean;
     bool show_info;
-    bool flip_normals;
     bool delete_faces;
 };
 
@@ -36,8 +36,8 @@ Arguments parse_args(int argc, char **argv) {
     args.set_description("Conversion app for meshes a la image magicks convert");
     args.add_option('t', "transform", true, "transform vertices with matrix file");
     args.add_option('i', "invert", false, "invert transform");
+    args.add_option('c', "clean", false, "cleanup mesh");
     args.add_option('\0', "show-info", false, "show info");
-    args.add_option('f', "flip-normals", false, "flip vertex normals");
     args.add_option('\0', "ascii", false, "write out ascii file");
     args.add_option('\0', "delete-faces", false, "delete faces");
     args.add_option('\0', "delete-vnormals", false, "delete vertex normals");
@@ -50,8 +50,8 @@ Arguments parse_args(int argc, char **argv) {
     conf.in_mesh = args.get_nth_nonopt(0);
     conf.out_mesh = args.get_nth_nonopt(1);
     conf.invert = false;
+    conf.clean = false;
     conf.show_info = false;
-    conf.flip_normals = false;
     conf.delete_faces = false;
 
     conf.opts.format_binary = true;
@@ -71,8 +71,8 @@ Arguments parse_args(int argc, char **argv) {
         case 'i':
             conf.invert = true;
         break;
-        case 'f':
-            conf.flip_normals = true;
+        case 'c':
+            conf.clean = true;
         break;
         case '\0':
             if (i->opt->lopt == "ascii") {
@@ -114,6 +114,30 @@ int main(int argc, char **argv) {
     mesh->ensure_normals(true, false);
 
     std::vector<uint> & faces = mesh->get_faces();
+    if (args.clean) {
+        std::vector<math::Vec3f> const & normals = mesh->get_face_normals();
+        std::vector<uint> tmp(faces.size());
+
+        std::size_t w = 0;
+        for (std::size_t i = 0; i < normals.size(); ++i) {
+            math::Vec3f const & normal = normals[i];
+            if (normal.norm() > 0.0f) {
+                tmp[w++] = faces[3 * i + 0];
+                tmp[w++] = faces[3 * i + 1];
+                tmp[w++] = faces[3 * i + 2];
+            }
+        }
+        tmp.resize(w);
+
+        std::size_t num_del_faces = faces.size() - tmp.size();
+        std::swap(faces, tmp);
+
+        std::size_t num_del_verts = mve::geom::mesh_delete_unreferenced(mesh);
+        std::cout << "Deleted" << num_del_faces << " faces and "
+            << num_del_verts << " vertices" << std::endl;
+        mesh->ensure_normals(true, false);
+    }
+
     std::vector<math::Vec3f> & vertices = mesh->get_vertices();
     std::vector<math::Vec3f> & normals = mesh->get_vertex_normals();
 
@@ -140,12 +164,6 @@ int main(int argc, char **argv) {
 
         for (std::size_t i = 0; i < normals.size(); ++i) {
             normals[i] = T.mult(normals[i], 0.0f).normalize();
-        }
-    }
-
-    if (args.flip_normals) {
-        for (std::size_t i = 0; i < normals.size(); ++i) {
-            normals[i] = -normals[i];
         }
     }
 
